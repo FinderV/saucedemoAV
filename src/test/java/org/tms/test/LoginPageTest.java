045ba72d834e828e7bab1a6a7f591d02489903fb
Aliaksandr Sirotkin <Aliaksandr_Sirotkin@epam.com>
2022-04-23 22:50:37 +0300

    annotations added

--- src/test/java/org/tms/test/LoginPageTest.java
+++ src/test/java/org/tms/test/LoginPageTest.java
@@ -1,5 +1,9 @@
 package org.tms.test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.TmsLink;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,7 +19,11 @@ public class LoginPageTest extends BaseTest {
     loginPageService = new LoginPageService();
   }
 
-  @Test
+  @Test(description = "Login to https://www.saucedemo.com/")
+  @Description("Login to https://www.saucedemo.com / as standard_user")
+  @Link("https://github.com/Alexandr-Sirotkin")
+  @Issue("Alexandr-Sirotkin")
+  @TmsLink("Alexandr-Sirotkin")
   public void loginTest() {
     InventoryPage inventoryPage = loginPageService.login();
     String actualTextOfInventoryPage = inventoryPage.getTextOfNameOfMainPageSection();
